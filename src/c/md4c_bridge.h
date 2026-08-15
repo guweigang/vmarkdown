6c278088cb1263e013298ce00543066448510733
weigang <guweigang@bullsoft.org>
2026-08-15 18:26:47 +0800

    Fix MSVC linkage for md4c bridge callbacks

--- src/c/md4c_bridge.h
+++ src/c/md4c_bridge.h
@@ -3,12 +3,20 @@
 
 #include "md4c.h"
 
-int vmarkdown_enter_block(int type, void* detail, void* userdata);
-int vmarkdown_leave_block(int type, void* detail, void* userdata);
-int vmarkdown_enter_span(int type, void* detail, void* userdata);
-int vmarkdown_leave_span(int type, void* detail, void* userdata);
-int vmarkdown_text(int type, MD_CHAR* text, MD_SIZE size, void* userdata);
-void vmarkdown_debug_log(char* msg, void* userdata);
+#ifndef VMARKDOWN_EXPORT
+#ifdef VV_EXP
+#define VMARKDOWN_EXPORT VV_EXP
+#else
+#define VMARKDOWN_EXPORT extern
+#endif
+#endif
+
+VMARKDOWN_EXPORT int vmarkdown_enter_block(int type, void* detail, void* userdata);
+VMARKDOWN_EXPORT int vmarkdown_leave_block(int type, void* detail, void* userdata);
+VMARKDOWN_EXPORT int vmarkdown_enter_span(int type, void* detail, void* userdata);
+VMARKDOWN_EXPORT int vmarkdown_leave_span(int type, void* detail, void* userdata);
+VMARKDOWN_EXPORT int vmarkdown_text(int type, MD_CHAR* text, MD_SIZE size, void* userdata);
+VMARKDOWN_EXPORT void vmarkdown_debug_log(char* msg, void* userdata);
 
 int vmd_parse_to_v(const MD_CHAR* text, MD_SIZE size, unsigned flags, void* userdata);
 
